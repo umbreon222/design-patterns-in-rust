f57076ec78d4875cfc1cd3e7778bf31b6e9c3b6b
umbreon222 <umbreon222@gmail.com>
2023-02-06 21:02:30 -0600

    Added mediator pattern

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,17 @@ pub mod observer {
 
 pub mod mediator {
     // https://www.dofactory.com/net/mediator-design-pattern
+    pub trait Mediator<T> {
+        fn manage(&mut self, item: T, item_name: &String);
+    }
 }
+
 use std::collections::HashMap;
 use std::rc::Rc;
 use core::cell::RefCell;
 use command::Command;
 use observer::{ Observer, Subject };
+use mediator::Mediator;
 
 pub struct Light {
     state: bool,
@@ -44,26 +49,6 @@ impl Light {
     }
 }
 
-impl Subject<bool> for Light {
-    fn attach_observer(&mut self, observer_key: &String, observer: Box<dyn Observer<bool>>) {
-        self.observer_map.insert(observer_key.to_string(), observer);
-    }
-
-    fn detach_observer(&mut self, observer_key: &String) -> bool {
-        if !self.observer_map.contains_key(observer_key) {
-            return false;
-        }
-
-        self.observer_map.remove(observer_key);
-        return true;
-    }
-
-    fn notify_observers(&mut self) {
-        for observer in self.observer_map.values_mut() {
-            observer.on_subject_updated(&self.state);
-        }
-    }
-}
 
 /* <command pattern example> */
 pub struct LightOnCommand {
@@ -104,26 +89,88 @@ impl Remote {
 /* </command pattern example> */
 
 /* <observer pattern example> */
+impl Subject<bool> for Light {
+    fn attach_observer(&mut self, observer_key: &String, observer: Box<dyn Observer<bool>>) {
+        self.observer_map.insert(observer_key.to_string(), observer);
+    }
+
+    fn detach_observer(&mut self, observer_key: &String) -> bool {
+        if !self.observer_map.contains_key(observer_key) {
+            return false;
+        }
+
+        self.observer_map.remove(observer_key);
+        return true;
+    }
+
+    fn notify_observers(&mut self) {
+        for observer in self.observer_map.values_mut() {
+            observer.on_subject_updated(&self.state);
+        }
+    }
+}
+
 pub struct LightStateObserver {
     update_count: u8
 }
 
 impl Observer<bool> for LightStateObserver {
     fn on_subject_updated(&mut self, update_source: &bool) {
-        println!("Light was updated to {}", update_source);
+        let mut light_state = "off";
+        if *update_source {
+            light_state = "on"
+        }
+        println!("Light was switched {}", light_state);
         self.update_count += 1;
     }
 }
 /* </observer pattern example> */
 
+/* <mediator pattern example> */
+pub enum LightAction {
+    On,
+    Off
+}
+
+pub struct LightMediator {
+    light_map: HashMap<String, Rc<RefCell<Light>>>
+}
+
+impl Mediator<Light> for LightMediator {
+    fn manage(&mut self, light: Light, light_name: &String) {
+        self.light_map.insert(light_name.clone(), Rc::new(RefCell::new(light)));
+    }
+}
+
+impl LightMediator {
+    fn perform_light_action(&mut self, light_action: LightAction, light_name: &String) {
+        match self.light_map.get(light_name) {
+            Some(light) => {
+                let mut remote: Remote;
+                match light_action {
+                    LightAction::On => {
+                        remote = Remote { command: Box::new(LightOnCommand { light: light.clone() }) };
+
+                    },
+                    LightAction::Off => {
+                        remote = Remote { command: Box::new(LightOffCommand { light: light.clone() }) };
+                    }
+                }
+                remote.execute();
+            },
+            None => {}
+        }
+    }
+}
+/* </mediator pattern example> */
+
 fn main() {
-    let light = Rc::new(RefCell::new(Light { state: false, observer_map: HashMap::new() }));
+    let light_name = "light_1".to_string();
+    let mut light = Light { state: false, observer_map: HashMap::new() };
     let light_observer_key = "light_observer_1".to_string(); 
-    light.borrow_mut().attach_observer(&light_observer_key, Box::new(LightStateObserver { update_count: 0 }));
-    let on_command = Box::new(LightOnCommand { light: light.clone() });
-    let off_command = Box::new(LightOffCommand { light: light.clone() });
-    let mut remote = Remote { command: on_command };
-    remote.execute();
-    remote.set_command(off_command);
-    remote.execute();
+    light.attach_observer(&light_observer_key, Box::new(LightStateObserver { update_count: 0 }));
+    let mut light_mediator = LightMediator { light_map: HashMap::new() };
+    light_mediator.manage(light, &light_name);
+    light_mediator.perform_light_action(LightAction::On, &light_name);
+    light_mediator.perform_light_action(LightAction::Off, &light_name);
 }
